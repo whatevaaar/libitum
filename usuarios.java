403a07f75e32243fe16f1e351b44ffc50c63559b
imff <39280853+imff@users.noreply.github.com>
2018-06-01 22:08:55 -0500

    Create usuarios.java

---
+++ usuarios.java
@@ -0,0 +1,52 @@
+package modelo;
+
+public class usuarios {
+    private String nomUsuario;
+    private String nombre;
+    private String apPat;
+    private String apMat;
+    private String password;
+
+    public String getNomUsuario() {
+        return nomUsuario;
+    }
+
+    public void setNomUsuario(String nomUsuario) {
+        this.nomUsuario = nomUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApPat() {
+        return apPat;
+    }
+
+    public void setApPat(String apPat) {
+        this.apPat = apPat;
+    }
+
+    public String getApMat() {
+        return apMat;
+    }
+
+    public void setApMat(String apMat) {
+        this.apMat = apMat;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
