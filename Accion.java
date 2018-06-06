d209faa5d3a7f74fb76279243582d9cd5c324d0d
whatevaaar <emilianohidalgo314@gmail.com>
2018-06-06 15:28:20 -0500

    Update Accion.java

--- Accion.java
+++ Accion.java
@@ -11,7 +11,7 @@ import java.util.Random;
 public class Accion {
     //Listas de palabras
     private final List<String> palabrasMover = Arrays.asList("ir","caminar", "derrar","correr", "avanzar", "correr");
-    private final List<String> palabrasInteractuar = Arrays.asList("abrir", "cerrar","subir", "bajar", "salir", "entrar", "mover", "saltar");
+    private final List<String> palabrasInteractuar = Arrays.asList("abrir", "cerrar","subir", "bajar", "salir", "entrar", "mover", "saltar","soltar");
     private final List<String> palabrasPelear = Arrays.asList("golpear", "atacar", "pelear");
     private final List<String> palabrasObservar = Arrays.asList("observar", "mirar", "ver");
     private final List<String> palabrasRecolectar = Arrays.asList("recolectar", "recoger", "tomar","agarrar");
